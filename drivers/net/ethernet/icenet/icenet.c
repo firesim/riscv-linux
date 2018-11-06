7c076c7012daddbf601629a9b466cf4be7068652
Howard Mao <zhehao.mao@gmail.com>
2018-11-06 18:29:42 +0000

    move icenet to separate directory and split off skbuff code

--- drivers/net/ethernet/icenet/icenet.c
+++ drivers/net/ethernet/icenet/icenet.c
@@ -14,6 +14,8 @@
 #include <linux/netdevice.h>
 #include <linux/etherdevice.h>
 
+#include "skbuff.h"
+
 #define ICENET_NAME "icenet"
 #define ICENET_SEND_REQ 0
 #define ICENET_RECV_REQ 8
@@ -34,51 +36,6 @@
 #define DMA_PTR_ALIGN(p) ((typeof(p)) (__ALIGN_KERNEL((uintptr_t) (p), ALIGN_BYTES)))
 #define DMA_LEN_ALIGN(n) (((((n) - 1) >> ALIGN_SHIFT) + 1) << ALIGN_SHIFT)
 #define MACADDR_BYTES 6
-
-struct sk_buff_cq_entry {
-	struct sk_buff *skb;
-};
-
-struct sk_buff_cq {
-	struct sk_buff_cq_entry entries[CONFIG_ICENET_RING_SIZE];
-	int head;
-	int tail;
-};
-
-#define SK_BUFF_CQ_COUNT(cq) CIRC_CNT(cq.head, cq.tail, CONFIG_ICENET_RING_SIZE)
-#define SK_BUFF_CQ_SPACE(cq) CIRC_SPACE(cq.head, cq.tail, CONFIG_ICENET_RING_SIZE)
-
-static inline void sk_buff_cq_init(struct sk_buff_cq *cq)
-{
-	cq->head = 0;
-	cq->tail = 0;
-}
-
-static inline void sk_buff_cq_push(
-		struct sk_buff_cq *cq, struct sk_buff *skb)
-{
-	cq->entries[cq->head].skb = skb;
-	cq->head = (cq->head + 1) & (CONFIG_ICENET_RING_SIZE - 1);
-}
-
-static inline struct sk_buff *sk_buff_cq_pop(struct sk_buff_cq *cq)
-{
-	struct sk_buff *skb;
-
-	skb = cq->entries[cq->tail].skb;
-	cq->tail = (cq->tail + 1) & (CONFIG_ICENET_RING_SIZE - 1);
-
-	return skb;
-}
-
-static inline int sk_buff_cq_tail_nsegments(struct sk_buff_cq *cq)
-{
-	struct sk_buff *skb;
-
-	skb = cq->entries[cq->tail].skb;
-
-	return skb_shinfo(skb)->nr_frags + 1;
-}
 
 struct icenet_device {
 	struct device *dev;
