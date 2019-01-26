5045da4f679438ca215e21adb4b8e96949578458
Howard Mao <zhehao.mao@gmail.com>
2019-01-26 04:20:29 +0000

    enable RoCC accelerator

--- arch/riscv/kernel/process.c
+++ arch/riscv/kernel/process.c
@@ -76,7 +76,7 @@ void show_regs(struct pt_regs *regs)
 void start_thread(struct pt_regs *regs, unsigned long pc,
 	unsigned long sp)
 {
-	regs->sstatus = SR_SPIE /* User mode, irqs on */ | SR_FS_INITIAL;
+	regs->sstatus = SR_SPIE /* User mode, irqs on */ | SR_FS_INITIAL | SR_XS_INITIAL;
 	regs->sepc = pc;
 	regs->sp = sp;
 	set_fs(USER_DS);
